4f18b31cace0a401ca78f528129fd85cd892207e
sisi28888 <57200801+sisi28888@users.noreply.github.com>
2019-12-28 12:49:08 -0500

    Update views.py

--- squirrel/map/views.py
+++ squirrel/map/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+ifrom .models import squirrels
 
+
+def squirrels_map(request):
+    sightings=squirrels.objects.all()[:45]
+    
+    return render(request, 'map/map.html',{'sightings':sightings})
 # Create your views here.
