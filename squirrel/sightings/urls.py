aeda69d566b0eddca767e3cae92f13dbacdc208c
sisi28888 <57200801+sisi28888@users.noreply.github.com>
2019-12-28 12:52:15 -0500

    Create urls.py

---
+++ squirrel/sightings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls import url, include
+from . import views
+
+app_name='sightings'
+
+urlpatterns = [
+    path('', views.index, name = "index"),
+    path('add/',views.add,name = "add"),
+    path('stats/',views.stats,name='stats'),
+    path('update/',views.update,name='update'),
+]
